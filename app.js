56d0e4f3ca730031d50e734a31efefbe99fd63ff
younusraza909 <younusraza909@gmail.com>
2023-01-13 11:01:31 +0500

    our first tours api

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server side");
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
