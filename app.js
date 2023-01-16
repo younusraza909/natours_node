b0cbe0549e52dc496c830614dbb6b22847523119
younusraza909 <younusraza909@gmail.com>
2023-01-16 10:39:34 +0500

    get tour by id route

--- app.js
+++ app.js
@@ -17,6 +17,28 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  // we can make an optional params using ? at the end like :id?
+
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const tour = tours.find((t) => t.id === id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
