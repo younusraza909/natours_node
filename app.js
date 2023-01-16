9dc66fc5d6423b2aeee813ecda4677880595d59f
younusraza909 <younusraza909@gmail.com>
2023-01-16 10:30:27 +0500

    new route for post

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+// Express dont put body on request object by default so we have to add this middleware
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -12,6 +14,21 @@ app.get("/api/v1/tours", (req, res) => {
     data: {
       tours,
     },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
