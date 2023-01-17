76fc382c85c4e822e6a81c04b0f8759ef748dab5
younusraza909 <younusraza909@gmail.com>
2023-01-17 10:23:31 +0500

    Chaining Middleware

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkId } = require("../controllers/tourControllers");
+const { getAllTours, createTour, getTour, updateTour, deleteTour, checkId, checkBody } = require("../controllers/tourControllers");
 const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(getAllTours).post(checkBody, createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
