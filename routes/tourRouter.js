c88750cde0f920a08b11f5698eee82f64f25168e
younusraza909 <younusraza909@gmail.com>
2023-01-19 11:43:16 +0500

    Aliasing Routes

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -5,6 +5,7 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } = require('../controllers/tourControllers');
 
 const router = express.Router();
@@ -12,6 +13,8 @@ const router = express.Router();
 // Implementing Params Middleware
 // in these types of middleware we have 4 values instead of 3 we get our params in last value in function
 // router.param('id', checkId);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
