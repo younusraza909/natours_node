cec1c628608ca2e471a240e042063994959fe2e2
younusraza909 <younusraza909@gmail.com>
2023-01-21 21:23:33 +0500

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -124,6 +124,15 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// Aggregation Middleware
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
