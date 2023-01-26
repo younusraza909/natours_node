e9dc7ea1e41dba460f06a1b66ff5cabdbe54f5c6
younusraza909 <younusraza909@gmail.com>
2023-01-26 10:21:30 +0500

    Tour model fixed

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,7 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxLength: [40, 'A tour name must have less or equal than 40 characters'],
-      minLength: [40, 'A tour name must have more or equal than 10 characters'],
+      minLength: [10, 'A tour name must have more or equal than 10 characters'],
     },
     slug: {
       type: String,
