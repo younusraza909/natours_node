9d0fe943d1cf1595321e9c88fb80986b549794e8
younusraza909 <younusraza909@gmail.com>
2023-01-27 10:06:08 +0500

    Event for unhandled rejection

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ mongoose
   });
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+// There might be some promise in our code which get rejected but not get handled by us
+//  in node event we have a event that will trigger every time such type of scenario is created
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting Down....');
+  // we can directly use .exit but it will shutdown all request currently running and its a bad way
+  // so we first shut server gracefully than use process.exit
+  server.close(() => {
+    process.exit(1);
+  });
 });
