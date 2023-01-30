a2065e69e0fbf5010b6dba76506769d16ae0d6be
younusraza909 <younusraza909@gmail.com>
2023-01-30 10:46:48 +0500

    User Controller to create a signup

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require("../controllers/userControllers");
+const express = require('express');
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userControllers');
+const { signup } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route("/").get(getAllUsers).post(createUser);
-router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+// router.route('/signup').post(signup);
+router.post('/signup', signup);
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
